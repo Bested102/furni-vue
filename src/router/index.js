72a91693875d630434a74c4232088932ccdf5504
Omar Yasser <omar4444yasser@gmail.com>
2023-11-17 19:47:56 +0200

    switched to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ShopView from "../views/ShopView.vue";
 import BlogView from "../views/BlogView.vue";
@@ -7,7 +7,7 @@ import ServicesView from "../views/ServicesView.vue";
 import AboutView from "../views/AboutView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
